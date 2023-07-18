cf28f92cfbf01e6a7aaf494ac5bc60b534caf85f
MatiMac1976 <you@example.com>
2023-07-18 17:20:00 -0300

    commitjava

---
+++ js/cargaDeDatos.js
@@ -0,0 +1,13 @@
+const nombre = prompt('Ingrese nombre');
+const apellido = prompt('Ingrese apellido');
+const edad = prompt('Ingrese edad');
+
+const datos = {
+    nombre,
+    apellido,
+    edad,
+}
+
+console.log('ingreso:',nombre,apellido,edad)
+
+console.log('ingreso:',JSON.stringify)
---
+++ js/variable.js
@@ -0,0 +1,17 @@
+var miNombre = 'Matias';
+var miApellido = 'Maciel';
+var miSegundoNombre = 'Oscar';
+
+const altura = 176;
+const colorDeOjos = 'marrones';
+
+let colorDePelo = 'castaño';
+let peso = 95;
+
+/*saber el tipo de variable*/
+typeof(miApellido)
+
+
+const objetoPersona = {
+    name: 'Matias'
+};
